ee7e1f2e993ded6550c1b55fb336a97db046bb9d
gaelkyeo <gael@kyeo.fr>
2018-11-18 20:25:39 +0100

    Ps version Edit

--- config/settings.inc.php
+++ config/settings.inc.php
@@ -10,7 +10,7 @@ define('_PS_CACHE_ENABLED_', '0');
 define('_COOKIE_KEY_', 'OO7PXpoO9YeJ7Xsc5BooSXjTVUTrFsODVHS4PW9fnovEMfnpW3DUb0Si');
 define('_COOKIE_IV_', 'tJgtwmNr');
 define('_PS_CREATION_DATE_', '2018-10-01');
-define('_PS_VERSION_', '1.6.1.22');
+define('_PS_VERSION_', '1.6.1.23');
 define('_PS_DIRECTORY_', '/');
 define('_RIJNDAEL_KEY_', '2jwcPvHLefqYcD4WC5DXP4AfaqCGqzZb');
 define('_RIJNDAEL_IV_', 'Yum5uKwgSsdUNe/56vA/CQ==');
