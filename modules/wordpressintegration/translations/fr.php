fcf66b41b7f2382ecf1e369246390ab89294db51
Kyeo <contact@kyeo.fr>
2019-03-23 19:01:16 +0100

    Translation files granted

---
+++ modules/wordpressintegration/translations/fr.php
@@ -0,0 +1,25 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{wordpressintegration}prestashop>wordpressintegration_c758830570a0551862fe3126ebd1a525'] = 'Intégration WordPress';
+$_MODULE['<{wordpressintegration}prestashop>wordpressintegration_599c9cd510b0b231a188387f224400a4'] = 'Intégrez les derniers articles de votre blog WordPress dans votre boutique.';
+$_MODULE['<{wordpressintegration}prestashop>wordpressintegration_33c12befbbaa3b04443981b981652967'] = 'Valeur de configuration invalide';
+$_MODULE['<{wordpressintegration}prestashop>wordpressintegration_c888438d14855d7d96a2724ee9c306bd'] = 'Configuration enregistrée';
+$_MODULE['<{wordpressintegration}prestashop>wordpressintegration_f4f70727dc34561dfde1a3c529b6205c'] = 'Configuration';
+$_MODULE['<{wordpressintegration}prestashop>wordpressintegration_64b50b410cb84da5edaeef105669c17e'] = 'Titre du blog :';
+$_MODULE['<{wordpressintegration}prestashop>wordpressintegration_36a8d6bc2edc221057cad8161efa14d7'] = 'Adresse URL du blog WordPress :';
+$_MODULE['<{wordpressintegration}prestashop>wordpressintegration_f14e94779ed0ac8ef51a32ba1bda1b25'] = 'Nombre d\'articles à afficher :';
+$_MODULE['<{wordpressintegration}prestashop>wordpressintegration_92e2dfbad690581981c47e557911f30f'] = 'Nombre maximum de caractères à afficher avant de couper le texte :';
+$_MODULE['<{wordpressintegration}prestashop>wordpressintegration_8999614df2cbb13316688f9bdf260ba2'] = 'Afficher les images des articles';
+$_MODULE['<{wordpressintegration}prestashop>wordpressintegration_786c8183434bace21033e82c25335287'] = 'Veuillez suivre les instructions de la';
+$_MODULE['<{wordpressintegration}prestashop>wordpressintegration_55876228853abf632dec9346a4f372ec'] = 'documentation fournie';
+$_MODULE['<{wordpressintegration}prestashop>wordpressintegration_b20fd699cef4320fca982ab19f112cc3'] = 'afin de mettre en place la récupération automatique des images des articles.';
+$_MODULE['<{wordpressintegration}prestashop>wordpressintegration_c9cc8cce247e49bae79f15173ce97354'] = 'Enregistrer';
+$_MODULE['<{wordpressintegration}prestashop>wordpressintegration_630f6dc397fe74e52d5189e2c80f282b'] = 'Retour à la liste';
+$_MODULE['<{wordpressintegration}prestashop>info_6e82c6f210f49286713f6a6f17c73e9c'] = 'Afin que les images de couverture des articles s\'affichent correctement, veuillez d\'abord suivre les instructions dans la';
+$_MODULE['<{wordpressintegration}prestashop>info_55876228853abf632dec9346a4f372ec'] = 'documentation';
+$_MODULE['<{wordpressintegration}prestashop>info_1b242e10fed9874b357e874a272b60ae'] = '.';
+$_MODULE['<{wordpressintegration}prestashop>simple_xml_not_detected_2ff513b989547ff09179fa42e99213a8'] = 'Erreur détectée ! L\'extension SimpleXML n\'est pas chargée ; veuillez vérifier la configuration de votre serveur.';
+$_MODULE['<{wordpressintegration}prestashop>display_blog_posts_43340e6cc4e88197d57f8d6d5ea50a46'] = 'En savoir plus';
+$_MODULE['<{wordpressintegration}prestashop>display_blog_posts_f4b3a01a574adf5bd1786db82fb60972'] = 'Voir tous les articles';
